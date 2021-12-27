c89d8d9ea4cc66a5514ca0acaf3a354269c4ca95
wellersonpro <wellersonhospedagem@outlook.com>
2021-12-27 12:04:59 -0300

    While e Do While

---
+++ estruturas de controle/exemploWhileDoWhile.js
@@ -0,0 +1,22 @@
+// Exemplo WHILE
+
+let valor01 = 12;
+let valor02 = 10;
+
+while (valor01 <= valor02) {
+    console.log(`Valor atual: ${valor01}`);
+    valor01++
+}
+
+
+console.log('=========================');
+
+
+// Exemplo DO/WHILE
+
+let valor03 = 12;
+let valor04 = 10;
+
+do {
+    console.log(`Valor atual: ${valor03}`) // DO/WHILE garante pelo menos uma repetição.
+} while (valor03 <= valor04);
